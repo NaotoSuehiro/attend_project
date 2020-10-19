72f9f0f5b89e7ca3591ab0054908b62b51b4e883
NaotoSuehiro <zenigata0505@gmail.com>
2020-10-19 15:04:47 +0000

    一覧表

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -30,21 +30,21 @@ class LecturesController < ApplicationController
   def update
       @lecture = Lecture.find(params[:id])
     if @lecture.save
-        flash[:succece]="クラスが更新されました"
+        flash[:succece] = "クラスが更新されました"
         redirect_to @lecture
     else
-     flash.now[:danger]="クラスが更新できませんでした"
+     flash.now[:danger] = "クラスが更新できませんでした"
      　render :edit
     end
       
   end
 
   def destroy
-      @lectute=Lecture.find(paramas[:id])
+      @lectute=Lecture.find(params[:id])
       @lecture.destroy
       
       flash[:succece]="クラスが削除されました"
-      redirect_to lectures_path
+      redirect_to lectures_url
   end
   
   private
