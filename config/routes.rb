78bc1f07670cb8c984087ebaf4c96a041acc9a10
NaotoSuehiro <zenigata0505@gmail.com>
2020-10-20 14:40:50 +0000

    remove-professor

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   # root to: 'toppages#index'For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
   
- 
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
+  
   resources :users, only: [:index, :show, :new, :create]
-  resources :professors,only: [:index, :show, :new, :create]
   resources :lectures
   resources :sessions, only: [:new, :create, :destroy]
-  resources :sessions_professor, only: [:new, :create, :destroy]
-  get "new_session", to: "professors#new"
-  
+
 end
