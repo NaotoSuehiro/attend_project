7cb61bfe3e88dd529e722539777c94cee6691aa9
NaotoSuehiro <zenigata0505@gmail.com>
2020-11-04 14:26:17 +0000

    プロフィール写真

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
 
   def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
+     @user = User.find(params[:id])
   end
 
   def new
