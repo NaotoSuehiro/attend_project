f01917d507cee34cace7e618628e0079fc5ae7b6
NaotoSuehiro <zenigata0505@gmail.com>
2020-11-05 12:52:34 +0000

    carrierwave途中

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,7 @@
 class Lecture < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :comment, presence: true, length: { maximum: 200 }
-  #mount_uploader :room, ImageUploader
+  mount_uploader :room, ImageUploader
   
   has_many :messages
   has_many :users, through: :messages
