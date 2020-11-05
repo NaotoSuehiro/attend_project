c81145dd86f1c141130d489c5e19c7ad1f368044
NaotoSuehiro <zenigata0505@gmail.com>
2020-11-05 11:32:19 +0000

    メディアリストテスト

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,7 @@
 class Lecture < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :comment, presence: true, length: { maximum: 200 }
-  validates :room, presence: true, length: { maximum: 50 }
+  #mount_uploader :room, ImageUploader
   
   has_many :messages
   has_many :users, through: :messages
